13b7450d5bc6b2cd5e39ddbbd3cc00027dc952c1
sanvine <santhiyamm2025@gmail.com>
2025-04-30 16:01:04 +0530

    Update Login.js

--- Frontend/myapp/src/pages/Login.js
+++ Frontend/myapp/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8000/auth/login`;
+            const url = `https://deploy-nb69.onrender.com/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
