1958f0a0eeb21e20f203b987e20cf7777a1b5be4
sanvine <santhiyamm2025@gmail.com>
2025-04-30 16:01:25 +0530

    Update Signup.js

--- Frontend/myapp/src/pages/Signup.js
+++ Frontend/myapp/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8000/auth/signup`;
+            const url = `https://deploy-nb69.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
